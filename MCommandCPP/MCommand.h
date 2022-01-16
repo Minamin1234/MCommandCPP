d4508831555b8f12d7c541aa32cd4cf598c23ea4
Minamin <minamin37345@gmail.com>
2022-01-17 00:51:17 +0900

    ヘッダファイルMCommandを追加．

---
+++ MCommandCPP/MCommand.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class MCommand
+{
+private:
+protected:
+	
+public:
+	string Version = "v1.0";
+};
