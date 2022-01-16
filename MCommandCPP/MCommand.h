1da67e460d3514659a9f1d837b1437293566095d
Minamin <minamin37345@gmail.com>
2022-01-17 01:08:13 +0900

    MCommandのプロトタイプ宣言を追加．

--- MCommandCPP/MCommand.h
+++ MCommandCPP/MCommand.h
@@ -3,13 +3,26 @@
 #include <string>
 #include <vector>
 
+#include "MModule.h"
+
 using namespace std;
 
 class MCommand
 {
 private:
 protected:
-	
+	vector<MModule> Modules;
+	vector<string> DefaultCommands;
+	char ModuleSprt = ',';
+	char SprtInArgs = ',';
+
+	virtual void Initialize();
 public:
 	string Version = "v1.0";
+
+	void Run();
+	vector<string> DecodeArgs(string words);
+	void ShowAllModuleCommandInfo();
+	void ShowAllDefaultCommands();
+	void ExeciteCommand(vector<string> args);
 };
